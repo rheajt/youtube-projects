88b1b88f68428dea51ef364b02a8b1067b5e27cf
jordan rhea <rheajt@gmail.com>
2019-07-19 04:36:42 +0800

    removed the original repos

--- degitter.js
+++ degitter.js
@@ -1,27 +1,27 @@
 const degit = require('degit');
 
 const repos = [
-  'clasp-with-pug',
-  'comment-harvester',
-  'star-wars-crawl',
-  'google-classroom-roster-maker',
-  'monster-math-with-equatio',
-  'holiday-gift-exchange',
-  'color-theme-properties-service',
-  '6-day-cycle-schedule-builder',
-  'insert-from-drawings',
-  'stranger-things-alphabet-wall',
-  'digital-app-dice',
-  'classroom-twitter',
-  'summer-vacation-display',
-  'wonder-wall-display',
-  'email-summarizer',
-  'library-genre-quiz-app',
-  'easy-grader',
-  'ubd-unit-planner',
-  'caption-creator-for-google-drive',
-  'slides2drive',
-  'library-portfolio',
+  'clasp-with-pug',  //removed
+  'comment-harvester',  //removed
+  'star-wars-crawl',  //removed
+  'google-classroom-roster-maker',  //removed
+  'monster-math-with-equatio',  //removed
+  'holiday-gift-exchange',  //removed
+  'color-theme-properties-service',  //removed
+  '6-day-cycle-schedule-builder',  //removed
+  'insert-from-drawings',  //removed
+  'stranger-things-alphabet-wall',  //removed
+  'digital-app-dice',  //removed
+  'classroom-twitter',  //removed
+  'summer-vacation-display',  //removed
+  'wonder-wall-display',  //removed
+  'email-summarizer',  //removed
+  'library-genre-quiz-app',  //removed
+  'easy-grader',  //removed
+  'ubd-unit-planner',  //removed
+  'caption-creator-for-google-drive',  //removed
+  'slides2drive',  //removed
+  'library-portfolio',  //removed
 ];
 
 repos.forEach(repo => {
